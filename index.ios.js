6ed372f69918f2e48262db0182b357e126b835d0
Jonathan Gray <jonathanceddy@gmail.com>
2017-05-08 20:07:13 -0700

    initial commit with base setup

---
+++ index.ios.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import React, {Component} from 'react'
+import { AppRegistry, View, Text } from 'react-native'
+import App from './src'
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <View><Text>asdf;djfaei</Text></View>
+//     )
+//   }
+// }
+
+AppRegistry.registerComponent('Rummikub', () => App);
